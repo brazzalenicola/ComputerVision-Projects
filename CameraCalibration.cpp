acb737f5d224373e7e8a82086f8cbc293f39ae1b
Nicola Brazzale <brazzalenicola@gmail.com>
2020-07-14 21:25:20 +0200

    new version, not working atm

--- CameraCalibration.cpp
+++ CameraCalibration.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  lab2
-//
-//  Created by Nicola Brazzale on 02/04/2020.
-//  Copyright © 2020 Nicola Brazzale. All rights reserved.
-//
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -14,7 +6,6 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     
     cv::Mat input_img[58];
-    char c[90];
     cv::Size patternsize(6,5);
     int CHESSBOARD[2]={6,5};
     vector<cv::Point2f> inner_cor;
@@ -24,6 +15,12 @@ int main(int argc, const char * argv[]) {
     vector<vector<cv::Point2f>> points2d;
     vector<cv::Point2f> proj_2d;
     bool success;
+    cv::Size imageSize;
+    
+    //Load the images
+    cv::String folder = "data/checkerboard_images/*.png"; //path to the images
+    std::vector<cv::String> filenames;
+    cv::glob(folder, filenames);
     
     for(int i=0; i<CHESSBOARD[1]; i++){
         for(int j=0; j<CHESSBOARD[0]; j++){
@@ -31,26 +28,30 @@ int main(int argc, const char * argv[]) {
         }
     }
 
-     for(int i=1;i<=58;i++){
-         if (i==37) {
-             continue;
-         }
-         if(i<10){
-             sprintf(c,"/Users/brazzalenicola/Desktop/Computer Vision/lab2/data/checkerboard_images/000%d_color.png",i);
-         } else {
-             sprintf(c,"/Users/brazzalenicola/Desktop/Computer Vision/lab2/data/checkerboard_images/00%d_color.png",i);
-         }
-         input_img[i-1] = cv::imread(c);
-         success = findChessboardCorners(input_img[i-1], patternsize, inner_cor,cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE
-         + cv::CALIB_CB_FAST_CHECK);
-         if (success) {
-             //cornerSubPix(input_img[i], inner_cor, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 30, 0.1));
+    for (int i = 0; i < filenames.size(); ++i) {
+        std::cout <<"processing " <<filenames[i] << "\r";
+
+        cv::Mat src = cv::imread(filenames[i], 0);
+
+        imageSize = cv::Size(src.rows, src.cols);
+
+        if (src.empty()) {
+            std::cout << "Image not found\n";
+            return -1;
+        }
+
+        std::vector<cv::Point3f> corners; //vector for detected corners
+
+        success = findChessboardCorners(src, patternsize, inner_cor, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE+ cv::CALIB_CB_FAST_CHECK);
+        if (success) {
+             cv::cornerSubPix(src, inner_cor, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 30, 0.1));
              
              points2d.push_back(inner_cor);
              
              points3d.push_back(corners);
          }
-     }
+    }
+
 
     cv::Mat cameraMatrix,distCoeffs,R,T;
     cv::calibrateCamera(points3d, points2d, cv::Size(input_img[1].rows,input_img[1].cols), cameraMatrix, distCoeffs, R, T);
@@ -81,7 +82,7 @@ int main(int argc, const char * argv[]) {
     cout<<"Maximum error "<<max<<" at iteration "<<itM<<endl;
     cout<<"minimum error "<<min<<" at iteration "<<itm<<endl;
     
-    cv::Mat new_img = cv::imread("/Users/brazzalenicola/Desktop/Computer Vision/lab2/data/test_image.png");
+    cv::Mat new_img = cv::imread("data/test_image.png");
     cv::Mat new_cameraMatrix, dst;
     
     cv::Mat map1, map2, R_matrix;
